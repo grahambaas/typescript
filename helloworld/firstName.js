af88b51df160872d8f361787d8d82f3fc59639db
Graham Baas <graham.baas@gmail.com>
2025-03-07 12:47:21 -0800

    First commit

---
+++ helloworld/firstName.js
@@ -0,0 +1,3 @@
+var firstName = "Dylan"; // type string
+//firstName = 33; // attempts to re-assign the value to a different type
+console.log(firstName);
---
+++ helloworld/firstName.ts
@@ -0,0 +1,5 @@
+let firstName: string = "Dylan"; // type string
+
+//firstName = 33; // attempts to re-assign the value to a different type
+
+console.log(firstName);
